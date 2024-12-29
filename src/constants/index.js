8dcfc412cfb93b4517ffef7b890a79096679860c
Ajay P <ajaykumarak1761@gmail.com>
2024-12-29 16:09:02 +0530

    altered .gitignore

---
+++ src/constants/index.js
@@ -0,0 +1,55 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+
+export const HERO_CONTENT = `Hello! I'm an aspiring full-stack developer passionate about crafting dynamic and responsive web applications. Although I am at the beginning of my professional journey, I am eager to learn and grow in this field. I am familiar with modern technologies like React, Next.js, MongoDB, and MySQL, which empower me to build both front-end and back-end solutions. Additionally, I have a strong foundation in Java programming, which remains my strongest skill and a key asset in my problem-solving toolkit. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a self-motivated and enthusiastic individual exploring the fascinating world of full-stack development. My journey began with a love for problem-solving, which I honed through my expertise in Java programming and a growing interest in creating dynamic, user-friendly web applications. I am familiar with technologies like React, Next.js, MongoDB, and MySQL, and I enjoy experimenting with them to bring ideas to life. Though I'm just starting in this field, I am passionate about continuous learning and tackling new challenges that push my skills further.
+Beyond coding, I love diving into tech trends, exploring creative solutions, and contributing to meaningful projects. I am driven by a desire to grow as a developer and collaborate with like-minded individuals to build impactful software solutions`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Intern",
+    company: "Rooman Technologies",
+    description: `Rooman Technologies is a software training company, and I am currently gaining valuable experience as an intern. My responsibilities include: Learning and applying software development practices in real-world scenarios. Collaborating on small projects to enhance my understanding of full-stack development. Strengthening my programming and problem-solving skills with a focus on Java and web technologies`,
+    technologies: ["Kubernetes", "Docker", "React", "mongoDB"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Intern",
+    company: "TechByHeart",
+    description: `During my internship at TechbyHeart, I gained valuable experience working with a range of Amazon Web Services (AWS) tools and Kali Linux. I developed hands-on knowledge in deploying and managing cloud infrastructure using 'Amazon EC2', which allowed me to set up and scale virtual servers in the cloud. I also worked extensively with 'Amazon S3' for secure storage and retrieval of data, and I became proficient in managing user access and permissions through 'AWS IAM'. Additionally, I explored Kali Linux, a powerful tool for penetration testing and cybersecurity, gaining insights into ethical hacking practices and security assessments.`,
+    technologies: ["Amazon EC2 ", "Amazon S3", "AWS IAM", "Kali Linux"],
+  }
+];
+
+export const PROJECTS = [
+  {
+    title: "EduMentor",
+    image: project1,
+    description:
+      "An appplication that connects students with mentors for personalized guidance and career advice. The project includes features like live chats, scheduling, and progress tracking to facilitate effective learning and mentorship.",
+    technologies: ["HTML", "CSS", "React.js", "MongoDB"],
+  },
+  {
+    title: "Railway Reservation System",
+    image: project2,
+    description:
+      "An application that allows users to book, cancel, and manage train tickets online. The system includes features like seat availability, real-time updates, and secure payment options for a seamless travel experience.",
+    technologies: ["HTML", "CSS", "React.js", "MySQL"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Tailwind", "Framer"],
+  },
+];
+
+export const CONTACT = {
+  address: "K.V.G.College of Engineering Boys Hostel , Sullia, Dakshina-Kannada, 574321.",
+  phoneNo: "+91 7795966562 ",
+  email: "ajaykumarak1761@gmail.com",
+};
